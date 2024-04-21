28c7731350dcdb95e69cbbcf964aef50035c2f2e
Yoga Bagas <yogabagas69@gmail.com>
2024-04-21 16:22:12 +0700

    Update URL in SecondaryFeatures component

--- src/components/SecondaryFeatures.jsx
+++ src/components/SecondaryFeatures.jsx
@@ -105,7 +105,6 @@ const features = [
       'A lot of templates, easy to use, and very stylish. Very cool. Fast in moment. You can use it anywhere without queuing.',
     image: screenshotContacts,
     url: 'https://photopal.id',
-    url: '',
     icon: function ContactsIcon() {
       return (
         <>
